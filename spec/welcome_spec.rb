b09331e8693f2139fb01aaf9450d8e3f3f35b273
Rob W <rob.whitehead@gmail.com>
2014-01-13 16:41:27 -0800

    Add lib folder

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,1 +1,10 @@
-welcome_spec.rb
+require "minitest/spec"
+require "minitest/autorun"
+require "welcome"
+
+describe Welcome do
+	it "has a message" do
+		hello=Welcome.new
+		hello.message.must_match "Welcome"
+	end
+end
