b3c032119712e53668c306b9f4c5524c5aad5038
robw <rob.whitehead@gmail.com>
2014-05-13 19:51:47 -0700

    cleanup post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,19 +2,16 @@ class Post < ActiveRecord::Base
 	  belongs_to :author, class_name: "User"
 
   def self.create_from_postmark(mitt)
-    api_token = mitt.from #.split("@").first.split("+").last
+    api_token = mitt.from
     user = User.find_by_email(api_token)
     if user
       post = user.posts.new
-      #binding.pry
-      #post.message_id = mitt.message_id
       post.title      = mitt.subject
       post.body = if mitt.text_body.blank?
         mitt.html_body
       else
         mitt.text_body
       end
-      # post.photo = mitt.attachments.first.read unless mitt.attachments.empty?
       post.save
     else
       return false
